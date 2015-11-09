011d6ee75f8be87538799b4395cc465074321423
pi24 <blaise@overpi.com>
2015-11-09 18:50:38 -0500

    Update and rename LICENSE.md to c++_multiples.cpp

---
+++ c++_multiples.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int x;
+   int z = 1;
+   cout << "enter your number" <<endl;
+   cin >> x;
+   cout << "your numbers" << endl;
+   while( z <= x)
+   {
+        if(z%7 == 0|| z%5 == 0)
+        {
+            cout << z << endl;
+        }
+        z++;
+    }
+} 
+
